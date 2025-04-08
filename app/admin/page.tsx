3f910af4bc232c8d519581106c99678a0879dce7
Gabekross <olugabriel80@gmail.com>
2025-04-08 01:40:40 -0400

    delete global.css

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+// /app/admin/page.tsx
+export default function AdminHomePage() {
+    return (
+      <div style={{ padding: '2rem' }}>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome to the admin panel.</p>
+      </div>
+    )
+  }
+  
